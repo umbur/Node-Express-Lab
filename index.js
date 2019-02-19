2cd8055e17e62cd41a32cf6354a7192dab66b5bd
umbur <work1415@gmail.com>
2019-02-19 16:02:38 -0700

    Created POST endpoint with the ability to sent info inside the request body

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 // Returns an array of all the post objects contained in the database.
 
 server.get("/api/posts", (req, res) => {
@@ -35,5 +37,34 @@ server.get("/api/posts/:id", (req, res) => {
       res.status(500).json({ error: "The post information could not be retrieved." })
     );
 });
+
+// Creates a post using the information sent inside the request body.
+
+server.post("/api/posts", (req, res) => {
+    const postInfo = req.body;
+  
+    if (!postInfo.title || !postInfo.contents) {
+      return res
+        .status(400)
+        .json({
+          errorMessage: "Please provide title and contents for the post."
+        });
+    }
+  
+    db.insert(postInfo).then(result => {
+      db.findById(result.id)
+        .then(post => {
+          res.status(201).json(post);
+        })
+        .catch(err =>
+          res
+            .status(500)
+            .jason({
+              error: "There was an error while saving the post to the database"
+            })
+        );
+    });
+  });
+
 
 server.listen(4000, () => console.log("server running on porn 4000"));
